3db99558a4997ff10bd303257f19b7e91a0e0283
David Hart <david@atipik.ch>
2024-12-11 11:44:52 +0100

    findAndCount wrong count with populate of ManyToOne then OneToMany with filter

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,31 +1,81 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import {
+  Collection,
+  Entity,
+  Filter,
+  ManyToOne,
+  MikroORM,
+  OneToMany,
+  PrimaryKey,
+  Property,
+  Ref,
+} from "@mikro-orm/sqlite";
 
 @Entity()
-class User {
-
+class Author {
   @PrimaryKey()
   id!: number;
 
   @Property()
   name: string;
 
-  @Property({ unique: true })
-  email: string;
+  @OneToMany(() => Book, (book) => book.author)
+  books = new Collection<Book>(this);
 
-  constructor(name: string, email: string) {
+  @OneToMany(() => Fan, (fan) => fan.favoriteAuthor)
+  fans = new Collection<Fan>(this);
+
+  constructor(name: string) {
     this.name = name;
-    this.email = email;
   }
+}
 
+@Entity()
+class Fan {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name: string;
+
+  @ManyToOne(() => Author)
+  favoriteAuthor!: Ref<Author>;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+@Entity()
+@Filter({
+  name: "soft-delete",
+  cond: { deletedAt: null },
+  default: true,
+})
+class Book {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  title: string;
+
+  @ManyToOne(() => Author)
+  author!: Ref<Author>;
+
+  @Property({ nullable: true })
+  deletedAt: Date | null = null;
+
+  constructor(title: string) {
+    this.title = title;
+  }
 }
 
 let orm: MikroORM;
 
 beforeAll(async () => {
   orm = await MikroORM.init({
-    dbName: ':memory:',
-    entities: [User],
-    debug: ['query', 'query-params'],
+    dbName: ":memory:",
+    entities: [Author, Book],
+    debug: ["query", "query-params"],
     allowGlobalContext: true, // only for testing
   });
   await orm.schema.refreshDatabase();
@@ -35,17 +85,20 @@ afterAll(async () => {
   await orm.close(true);
 });
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
+test("findAndCount", async () => {
+  const author = orm.em.create(Author, { name: "Stephen King" });
+  orm.em.create(Fan, { favoriteAuthor: author, name: "David" });
+  orm.em.create(Fan, { favoriteAuthor: author, name: "Jeremy" });
+  orm.em.create(Book, { author, title: "Book 1", deletedAt: null });
+  orm.em.create(Book, { author, title: "Book 2", deletedAt: null });
   await orm.em.flush();
   orm.em.clear();
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
-
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+  const [fans, count] = await orm.em.findAndCount(
+    Fan,
+    {},
+    { populate: ["favoriteAuthor.books"] }
+  );
+  expect(fans).toHaveLength(2);
+  expect(count).toBe(2);
 });
